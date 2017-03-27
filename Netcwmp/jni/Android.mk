e49fa0a5532d61af7bc3562f58ee4c027b301cd1
superjang <superjang@163.com>
2017-03-27 17:30:14 +0800

    use static lib insdead c file

--- Netcwmp/jni/Android.mk
+++ Netcwmp/jni/Android.mk
@@ -3,39 +3,33 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := pool
-LOCAL_SRC_FILES := libpool/src/pool.c 
+LOCAL_SRC_FILES := libpoo.a 
 
-include $(BUILD_STATIC_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := xmlet
-LOCAL_SRC_FILES := libxmlet/src/attr.c libxmlet/src/node.c libxmlet/src/element.c libxmlet/src/parser.c \
-libxmlet/src/buffer.c libxmlet/src/document.c libxmlet/src/list.c libxmlet/src/namemap.c libxmlet/src/xmlet.c
 
-LOCAL_CFLAGS += -DUSE_CWMP_MEMORY_POOL
-LOCAL_LDLIBS := \
-	-lz \
-	-lm 
+
+LOCAL_SRC_FILES := libxmlet.a 
+
+
 LOCAL_SHARED_LIBRARIES := libpool
 
-include $(BUILD_STATIC_LIBRARY)
+include $(RREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cwmp
-LOCAL_SRC_FILES := libcwmp/src/buffer.c libcwmp/src/cfg.c libcwmp/src/cwmp.c libcwmp/src/http.c libcwmp/src/ssl.c \
-libcwmp/src/ini.c libcwmp/src/log.c libcwmp/src/md5.c libcwmp/src/memory.c libcwmp/src/session.c libcwmp/src/util.c \
-libcwmp/src/event.c libcwmp/src/queue.c libcwmp/src/model.c libcwmp/src/callback.c
-LOCAL_SHARED_LIBRARIES := libpool libxmlet
-LOCAL_CFLAGS += -DUSE_CWMP_MEMORY_POOL
-LOCAL_LDLIBS := \
-	-lz \
-	-lm 
-	
-include $(BUILD_STATIC_LIBRARY)
 
+LOCAL_SRC_FILES := libcwmp.a 
+
+LOCAL_SHARED_LIBRARIES := libpool libxmlet
+	
+
+include $(RREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
